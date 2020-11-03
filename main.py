a9e60a43815a7596646b9ec8fc246669e15bdaec
inyourface3445 <trenten_miller@etownstudents.org>
2020-11-03 20:13:20 +0000

    re-did evrything

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 from colorama import Fore
 red = Fore.LIGHTGREEN_EX
 green = Fore.RED
-# orange = Fore.PURPLE
 prime_counter = 0
 print(
-    "The numbers that are in green are the ones that are prime. The ones that are in red are not primes. PLS upvote if you think it is a good program"
+    "The numbers that are in green are the ones that are prime. The ones that are in red are not primes. PLS upvote if you think it is a good program (most of the credit should go to https://www.programiz.com/python-programming/examples/prime-number)"
 )
-print(str(10))
-# print(float('inf'))
-# bottem = 0
-# high = float('inf')
-def str_to_int(num_to_con):
-  num_con = str(num_to_con)
-  return num_con
+
+# user input
 bottem = int(input(Fore.GREEN + "Enter a low number (no negatives pls): "))
 high = int(input(Fore.YELLOW + "Enter a high number: "))
-show_factors = str(input(Fore.BLUE + "do you want to see the factors of all of the numbers? (y/n) "))
+show_factors = str(
+    input(Fore.BLUE +
+          "do you want to see the factors of all of the numbers? (y/n) "))
+y_n_logging = str(input(Fore.BLUE + "do you want logging? (y/n)"))
+if (y_n_logging == 'y'):
+    logging_file = str(
+        input(
+            Fore.GREEN +
+            "enter the name ofthe file that you want to log to (type none for no loging) "
+        ))
+
+
+# functions
+def str_to_int(num_to_con):
+    num_con = str(num_to_con)
+    return num_con
+
+
+def loging(ver_to_log):
+    with open(logging_file, 'a') as x:
+        x.write(ver_to_log)
+
+
+# actual math
 for num in range(bottem, high):
     if num > 1:
         for i in range(2, num):
@@ -23,15 +40,15 @@ for num in range(bottem, high):
                 print(green, num, "is not a prime number")
                 print(i, "times", num // i, "is", num)
                 if (show_factors == 'y'):
-                  print('lowest factor:', i)
+                    print('lowest factor:', i)
                 break
         else:
             print(red, num, "is a prime number")
-            with open('primes.txt', 'w') as x:
-              x.write(str_to_int(num))
-            x.close()
+            string = str_to_int(num)
+            if (logging_file != 'none'):
+                loging(str_to_int(num) + '\n')
             prime_counter = prime_counter + 1
             print('how many primes that i found:', prime_counter)
-
     else:
         print(green, num, "is not a prime number")
+loging('number of primes found:' + str_to_int(prime_counter))
