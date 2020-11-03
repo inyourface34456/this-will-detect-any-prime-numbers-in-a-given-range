9981b5eef30b6a88721d4c0512225f55315b2c2d
inyourface3445 <trenten_miller@etownstudents.org>
2020-11-03 17:21:16 +0000

    added a feature that you can chose to see the lowest factor of a number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from colorama import Fore
 red = Fore.LIGHTGREEN_EX
 green = Fore.RED
+# orange = Fore.PURPLE
 prime_counter = 0
 print(
     "The numbers that are in green are the ones that are prime. The ones that are in red are not primes. PLS upvote if you think it is a good program"
@@ -10,12 +11,15 @@ print(
 # high = float('inf')
 bottem = int(input(Fore.GREEN + "Enter a low number (no negatives pls): "))
 high = int(input(Fore.YELLOW + "Enter a high number: "))
+show_factors = str(input(Fore.BLUE + "do you want to see the factors of all of the numbers? (y/n) "))
 for num in range(bottem, high):
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
                 print(green, num, "is not a prime number")
                 print(i, "times", num // i, "is", num)
+                if (show_factors == 'y'):
+                  print('lowest factor:', i)
                 break
         else:
             print(red, num, "is a prime number")
