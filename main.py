b298bea39d81f183144be18ae4bfbc007cd08e95
inyourface3445 <trenten_miller@etownstudents.org>
2020-11-03 17:34:31 +0000

    added logs

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ prime_counter = 0
 print(
     "The numbers that are in green are the ones that are prime. The ones that are in red are not primes. PLS upvote if you think it is a good program"
 )
+print(str(10))
 # print(float('inf'))
 # bottem = 0
 # high = float('inf')
+def str_to_int(num_to_con):
+  num_con = str(num_to_con)
+  return num_con
 bottem = int(input(Fore.GREEN + "Enter a low number (no negatives pls): "))
 high = int(input(Fore.YELLOW + "Enter a high number: "))
 show_factors = str(input(Fore.BLUE + "do you want to see the factors of all of the numbers? (y/n) "))
@@ -23,6 +27,9 @@ for num in range(bottem, high):
                 break
         else:
             print(red, num, "is a prime number")
+            with open('primes.txt', 'w') as x:
+              x.write(str_to_int(num))
+            x.close()
             prime_counter = prime_counter + 1
             print('how many primes that i found:', prime_counter)
 
